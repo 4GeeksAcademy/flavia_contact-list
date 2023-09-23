f2c650bc000b0a2119c741cc085ccb51679be708
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-09-23 23:09:47 +0000

    style: :art: Vista home estilizada

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -14,33 +14,55 @@ export const Home = () => {
     window.location.href = `tel:${phoneNumber}`;
   };
 
+  const pairs = store.contacts.reduce((result, item, index) => {
+    if (index % 2 === 0) result.push([]);
+    result[result.length - 1].push(item);
+    return result;
+  }, []);
+
   return (
     <div className="container">
-      <div className="contactContainer">
-        <div className="data">
-          {store.contacts.map((contact, index) => {
-            return (
-              <>
-                <li key={index}>
-                  <img src={contact.address} />
-                  {contact.full_name}, {contact.email}, {contact.phone}
-                </li>
-                <button onClick={() => actions.deleteContact(contact.id)}>
-                  Eliminar
-                </button>
-                <Link to={`/single/${contact.id}`}>Ir al perfil</Link>
-                <button onClick={() => sendEmail(contact.email)}>
-                  Enviar Correo
-                </button>
-                <button onClick={() => callPhoneNumber(contact.phone)}>
-                  Llamar
-                </button>
-                <br />
-              </>
-            );
-          })}
+      {pairs.map((pair, index) => (
+        <div key={index} className="row">
+          {pair.map((contact, contactIndex) => (
+            <div key={contactIndex} className="col">
+              <div className="contactContainer">
+                <div className="infoContainer">
+                  <div className="imgContainer">
+                    <img src={contact.address} alt={contact.full_name} />
+                  </div>
+                  <div className="nameContainer">{contact.full_name}</div>
+                  <div className="buttonsContainer">
+                    <button
+                      onClick={() => callPhoneNumber(contact.phone)}
+                      className="icon"
+                    >
+                      <i className="fa-solid fa-phone"></i>
+                    </button>
+                    <button
+                      onClick={() => sendEmail(contact.email)}
+                      className="icon"
+                    >
+                      <i className="fa-solid fa-envelope-open-text"></i>
+                    </button>
+                    <button className="icon">
+                      <Link to={`/single/${contact.id}`}>
+                        <i className="fa-regular fa-address-card"></i>
+                      </Link>
+                    </button>
+                    <button
+                      onClick={() => actions.deleteContact(contact.id)}
+                      className="icon"
+                    >
+                      <i className="fa-solid fa-trash-can"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-      </div>
+      ))}
     </div>
   );
 };
