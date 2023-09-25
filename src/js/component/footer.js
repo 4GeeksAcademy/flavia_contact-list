c283ae63686808995ec58a2069feac52266f03ca
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-09-25 17:31:17 +0000

    fix: :lipstick: Footer con característica fixed y modificado valores media query en vista home

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -2,10 +2,13 @@ import React, { Component } from "react";
 import "../../styles/footer.css";
 
 export const Footer = () => (
-  <footer className="footer mt-auto py-3 text-center">
+  <footer className="footer mt-auto py-3 text-center fixed-bottom">
     <p>
       Made with <i className="fa fa-heart text-danger" /> by{" "}
-      <a href="https://www.linkedin.com/in/flavia-fernandezolivera/" className="linkedin">
+      <a
+        href="https://www.linkedin.com/in/flavia-fernandezolivera/"
+        className="linkedin"
+      >
         Flavia Olivera
       </a>
     </p>
